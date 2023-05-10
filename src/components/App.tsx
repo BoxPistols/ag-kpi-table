3db298edea9792d4c7d7a863117fef9ee68c9e7e
aito <BoxPistols@users.noreply.github.com>
2023-05-11 07:18:48 +0900

    style to normalize

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,39 @@
+"use client"
 import "@/styles.css"
 import { KpiTable } from "./KpiTable"
+import { CssBaseline, Typography } from "@mui/material"
 
 export default function App() {
   return (
     <>
+      <CssBaseline />
+
+      <Typography
+        variant="h5"
+        align="center"
+        sx={{
+          backgroundColor: "#3e71c5f3",
+          color: "#fff",
+          padding: "8px",
+          position: "sticky",
+          top: 0,
+          left: 0,
+          zIndex: 100,
+        }}
+      >
+        AgGrid Get Data from API
+      </Typography>
+      <Typography
+        variant="h6"
+        border={"1px solid #eee"}
+        sx={{
+          py: 1,
+          px: 2,
+        }}
+      >
+        <Typography variant="h5">KPI APP</Typography>
+      </Typography>
       <div className="App">
-        <h2>KPI APP</h2>
         <KpiTable />
       </div>
     </>
--- src/components/KpiList.tsx
+++ src/components/KpiList.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { Box, Select, MenuItem, Typography } from "@mui/material"
+import { Box, Select, MenuItem, Typography, Divider } from "@mui/material"
 import { AgGridReact } from "ag-grid-react"
 import Data from "@/data.json"
 import "ag-grid-community/styles/ag-grid.css"
@@ -73,96 +73,111 @@ export const KpiList = () => {
 
   return (
     <>
-      <Box
-        display="grid"
-        gap={3}
-        sx={{
-          gridTemplateColumns: "minmax(180px, 1fr) minmax(120px, 1fr) minmax(200px, 800px)",
-          gridTemplateRows: "80px auto",
-        }}
-        width="100%"
-      >
-        <Box sx={{ gridColumn: "1 / 2", gridRow: "1 / 3" }}>
-          <Typography variant="h6">特徴量セットリスト</Typography>
-          {selectedParent && (
-            <List>
-              {selectedParent.children.map((child: Child, index: number) => (
-                <ListItem button key={index} onClick={() => handleChildClick(child)}>
-                  <ListItemText primary={child.title} />
-                </ListItem>
-              ))}
-            </List>
-          )}
-        </Box>
+      {/* Parent List */}
+      <Box overflow={"hidden"}>
+        {selectedParent && (
+          <List
+            sx={{
+              overflowY: "scroll",
+              maxHeight: "84vh",
+              height: "100%",
+              border: "1px solid #eee",
+              m: 1,
+              whiteSpace: "nowrap",
+              overflowX: "hidden",
+            }}
+          >
+            {selectedParent.children.map((child: Child, index: number) => (
+              <ListItem
+                button
+                key={index}
+                onClick={() => handleChildClick(child)}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <ListItemText
+                  primary={child.title}
+                  sx={{
+                    "&.MuiListItemText-root>.MuiTypography-root": {
+                      fontSize: 14,
+                    },
+                  }}
+                />
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </Box>
 
-        <Box sx={{ gridColumn: "2 / 4", gridRow: "1 / 1" }}>
-          <Box display="flex" flexDirection="column" width="100%">
-            <Typography variant="body1">選択されたParentとChildに応じたテーブル</Typography>
-            <Select
-              native
-              value={selectedParent ? selectedParent.id : ""}
-              onChange={handleParentSelect}
-              displayEmpty
-              sx={{ maxWidth: 800 }}
-            >
-              <option value="">-- KPIを選択 --</option>
-              {parents.map((parent) => (
-                <option key={parent.id} value={parent.id}>
-                  {parent.name}
-                </option>
+      {/* Child List */}
+      <Box sx={{ gridColumn: "2 / 4", gridRow: "1 / 1" }}>
+        <Box display="flex" flexDirection="column" width="100%">
+          <Typography variant="body1">選択されたParentとChildに応じたテーブル</Typography>
+          <Select
+            native
+            value={selectedParent ? selectedParent.id : ""}
+            onChange={handleParentSelect}
+            displayEmpty
+            sx={{ maxWidth: 800 }}
+          >
+            <option value="">-- KPIを選択 --</option>
+            {parents.map((parent) => (
+              <option key={parent.id} value={parent.id}>
+                {parent.name}
+              </option>
+            ))}
+            <>
+              {kpis.map((item) => (
+                <React.Fragment key={item.id}>{item.name}</React.Fragment>
               ))}
+            </>
+          </Select>
+          <Box sx={{ background: "#eee", width: "100%", height: "100%" }} px={1} py={0.5}>
+            <Typography variant="h6">
               <>
-                {kpis.map((item) => (
-                  <React.Fragment key={item.id}>{item.name}</React.Fragment>
-                ))}
+                {selectedParent?.name} - {selectedChild?.title}
               </>
-            </Select>
-            <Box sx={{ background: "#eee", width: "100%", height: "100%" }} px={1} py={0.5}>
-              <Typography variant="h6">
-                <>
-                  {selectedParent?.name} - {selectedChild?.title}
-                </>
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* Column Names List */}
+      <Box mt={3}>
+        <Box sx={{ overflowY: "auto", maxHeight: 720 }}>
+          <Typography variant="h6">カラム一覧</Typography>
+          <Typography variant="caption">({columnCount} カラム)</Typography>
+          {selectedParent &&
+            selectedParent.columns.map((name, index) => (
+              <Typography key={index} variant="body1">
+                {name}
               </Typography>
-            </Box>
-          </Box>
+            ))}
         </Box>
+      </Box>
 
-        <Box sx={{ gridColumn: "2 / 2", gridRow: "2 / 2" }} mt={3}>
-          <Box sx={{ overflowY: "auto", maxHeight: 720 }}>
-            {/* Column Names List */}
-            <Typography variant="h6">カラム一覧</Typography>
-            <Typography variant="caption">({columnCount} カラム)</Typography>
-
-            {selectedParent &&
-              selectedParent.columns.map((name, index) => (
-                <Typography key={index} variant="body1">
-                  {name}
-                </Typography>
-              ))}
-          </Box>
-        </Box>
-
-        {/* Table */}
-        <Box sx={{ gridColumn: "3 / 3", gridRow: "2 / 2" }} mt={3}>
-          <Box display="flex" flexDirection="column">
-            {/* Column Names List */}
-
-            <Box sx={{ height: 600 }}>
-              {selectedChild ? (
-                <>
-                  <h3>
-                    {selectedParent?.name} - {selectedChild.title}
-                  </h3>
-                  <div className="ag-theme-alpine" style={{ height: "100%", width: "100%" }}>
-                    <AgGridReact rowData={rowData} columnDefs={columnDefs} defaultColDef={{ resizable: true }} />
-                  </div>
-                </>
-              ) : selectedParent ? (
-                <p>特徴要素を選択してください</p>
-              ) : (
-                <p>KPIを選択してください</p>
-              )}
-            </Box>
+      {/* Table */}
+      <Box mt={3}>
+        <Box display="flex">
+          {/* Column Names List */}
+          <Box sx={{ height: 600 }}>
+            {selectedChild ? (
+              <>
+                <h3>
+                  {selectedParent?.name} - {selectedChild.title}
+                </h3>
+                <div className="ag-theme-alpine" style={{ height: "100%", width: "100%" }}>
+                  <AgGridReact rowData={rowData} columnDefs={columnDefs} defaultColDef={{ resizable: true }} />
+                </div>
+              </>
+            ) : selectedParent ? (
+              <p>特徴要素を選択してください</p>
+            ) : (
+              <p>KPIを選択してください</p>
+            )}
           </Box>
         </Box>
       </Box>
